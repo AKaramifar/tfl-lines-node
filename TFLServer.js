329c02b7fa843ecd13fe20dd9dcbae1e0fa58270
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-06-24 18:52:19 +0100

    update

--- TFLServer.js
+++ TFLServer.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
