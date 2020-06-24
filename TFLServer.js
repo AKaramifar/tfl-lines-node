0c6d8eb3fa32e1961d8fe2ad43d7943f887d9606
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-06-24 15:01:04 +0100

    update

--- TFLServer.js
+++ TFLServer.js
@@ -1,35 +1,46 @@
 const express = require("express");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const app = express();
 
-app.get('/developer',(req, res) => {
-    res.send('Afshin Karamifar')
-})
+app.get("/developer", (req, res) => {
+  res.send("Afshin Karamifar");
+});
 app.get("/mode", (req, res) => {
-    fetch("https://api.tfl.gov.uk/Line/Meta/Modes")
-      .then((Response) => {
-        return Response.json();
-      })
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((error) => console.log(error));
-  });
-  app.get("/vehicle", (req, res) => {
-    const {vehicle} = req.query;
-    fetch(`https://api.tfl.gov.uk/Line/Mode/${vehicle}`)
-      .then((Response) => {
-        return Response.json();
-      })
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((error) => console.log(error));
-  });
+  fetch("https://api.tfl.gov.uk/Line/Meta/Modes")
+    .then((Response) => {
+      return Response.json();
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => console.log(error));
+});
+app.get("/vehicle", (req, res) => {
+  const { vehicle } = req.query;
+  fetch(`https://api.tfl.gov.uk/Line/Mode/${vehicle}`)
+    .then((Response) => {
+      return Response.json();
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => console.log(error));
+});
 
-  //Listen to port 3000 or any available
+app.get("/route", (req, res) => {
+  const { route } = req.query;
+  fetch(`https://api.tfl.gov.uk/Line/${route}/Route`)
+    .then((Response) => {
+      return Response.json();
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => console.log(error));
+});
+//Listen to port 3000 or any available
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`)
-})
+  console.log(`Server started on port: ${PORT}`);
+});
